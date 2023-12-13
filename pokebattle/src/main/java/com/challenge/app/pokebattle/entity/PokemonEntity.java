776303d17c9b75ef7e301e87fd65b0e2f6fb13cd
Andre <parelhoneves.andre@gmail.com>
2023-12-13 19:38:47 -0300

    add equals and hashCode to entity

--- pokebattle/src/main/java/com/challenge/app/pokebattle/entity/PokemonEntity.java
+++ pokebattle/src/main/java/com/challenge/app/pokebattle/entity/PokemonEntity.java
@@ -2,6 +2,8 @@ package com.challenge.app.pokebattle.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "pokemons")
 public class PokemonEntity {
@@ -42,5 +44,17 @@ public class PokemonEntity {
 
     public void setBase_experience(float base_experience) {
         this.base_experience = base_experience;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PokemonEntity that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getPokemonId(), that.getPokemonId()) && Objects.equals(getName(), that.getName()) && Objects.equals(getBase_experience(), that.getBase_experience());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPokemonId(), getName(), getBase_experience());
     }
 }
